ceb2455f2b83b50ed460829db1b5533bb0304ec3
amirqurbani-CDA23GBG <amirqurbani-CDA23GBG@DESKTOP-5LMPU65>
2023-09-11 09:58:14 +0200

    Add project files.

---
+++ Lesson7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Amir !");
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
